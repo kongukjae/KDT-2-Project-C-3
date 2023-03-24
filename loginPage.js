0eaa2fc194903eed592495f10942098059551547
sumin <ujou173@gmail.com>
2023-03-24 17:28:36 +0900

    loginPage 스타일링 객체화 / 퇴실 전 커밋

--- loginPage.js
+++ loginPage.js
@@ -1,16 +1,27 @@
 // root 생성
 
-// root 식별 및 스타일 설정
+// root 식별
 const rootLoginPage = document.getElementById('root');
-rootLoginPage.style.width = '500px';
-rootLoginPage.style.height = '100vh';
-rootLoginPage.style.display = 'flex';
-rootLoginPage.style.flexDirection = 'column';
-rootLoginPage.style.justifyContent = 'top';
-rootLoginPage.style.alignItems = 'center';
-rootLoginPage.style.backgroundColor = '#F7786B'
 
-//tag maker
+// root 스타일 설정
+const rootLoginPageStyleObj = {
+  width : '500px',
+  height : '100vh',
+  display : 'flex',
+  flexDirection : 'column',
+  justifyContent : 'top',
+  alignItems : 'center',
+  backgroundColor : '#F7786B'
+};
+let rootLoginPageArr = [];
+for(let key in rootLoginPageStyleObj) {
+  rootLoginPageArr.push(key);
+}
+for(let i = 0; i < rootLoginPageArr.length; i++) {
+  rootLoginPage.style[rootLoginPageArr[i]] = rootLoginPageStyleObj[rootLoginPageArr[i]];
+}
+
+//tagCreate 함수
 function tagCreate(tType,props){
   let element = document.createElement(tType);
   for(let i in props){
@@ -19,11 +30,20 @@ function tagCreate(tType,props){
   return element;
 };
 
-// 로고 영역
+// 로고 영역 생성
 const logoLoginPageWaper = tagCreate('div', '');
-logoLoginPageWaper.style.width = '250px';
-logoLoginPageWaper.style.height = '100px';
-logoLoginPageWaper.style.marginTop = '156px';
+const logoLoginPageWaperStyleObj = {
+  width : '250px',
+  height : '100px',
+  marginTop : '156px',
+};
+let logoLoginPageWaperStyleArr = [];
+for(let key in logoLoginPageWaperStyleObj) {
+  logoLoginPageWaperStyleArr.push(key);
+}
+for(let i = 0; i < logoLoginPageWaperStyleArr.length; i++) {
+  logoLoginPageWaper.style[logoLoginPageWaperStyleArr[i]] = logoLoginPageWaperStyleObj[logoLoginPageWaperStyleArr[i]];
+}
 rootLoginPage.appendChild(logoLoginPageWaper);
 
 const logoLoginPage = tagCreate('img', '');
@@ -33,17 +53,27 @@ logoLoginPageWaper.appendChild(logoLoginPage);
 
 
 
-// 로그인 페이지 이미지
+// 로그인 페이지 이미지 요소 생성
 const imgLoginPageWrapper = tagCreate('div', '');
-imgLoginPageWrapper.style.width = '150px';
-imgLoginPageWrapper.style.height = '150px';
-imgLoginPageWrapper.style.marginTop = '60px';
-imgLoginPageWrapper.style.borderRadius = '50%';
-imgLoginPageWrapper.style.backgroundColor = '#d9d9d9';
-imgLoginPageWrapper.style.display = 'flex';
-imgLoginPageWrapper.style.justifyContent = 'center';
-imgLoginPageWrapper.style.alignItems = 'center';
-imgLoginPageWrapper.style.overflow = 'hidden';
+const imgLoginPageWrapperStyleObj = {
+  width : '150px',
+  height : '150px',
+  marginTop : '60px',
+  borderRadius : '50%',
+  backgroundColor : '#d9d9d9',
+  display : 'flex',
+  justifyContent : 'center',
+  alignItems : 'center',
+  overflow : 'hidden',
+}
+let imgLoginPageWrapperArr = [];
+for(let key in imgLoginPageWrapperStyleObj) {
+  imgLoginPageWrapperArr.push(key);
+}
+for(let i = 0; i <imgLoginPageWrapperArr.length; i++) {
+  imgLoginPageWrapper.style[imgLoginPageWrapperArr[i]] = imgLoginPageWrapperStyleObj[imgLoginPageWrapperArr[i]];
+}
+
 rootLoginPage.appendChild(imgLoginPageWrapper);
 
 const imgLoginPage = tagCreate('img', '');
@@ -60,7 +90,7 @@ rootLoginPage.appendChild(formLoginPageWrapper);
 
 formLoginPageWrapper.innerHTML += `
 <form action="/login" method="post">
-  <input type="text" name="user_id"></input>
+  <input type="text" name="user_id" autofocus></input>
   <input type="text" name="user_pw"></input>
   <div>
     <input type="submit" value="로그인">
@@ -69,22 +99,40 @@ formLoginPageWrapper.innerHTML += `
 </form>
 `;
 
-// form 태그
-formLoginPageWrapper.children[0].style.display = 'flex';
-formLoginPageWrapper.children[0].style.flexDirection = 'column';
-formLoginPageWrapper.children[0].style.marginTop = '110px';
+// form 태그 스타일
+const formStyleObj = {
+  display : 'flex',
+  flexDirection : 'column',
+  marginTop : '110px',
+};
+let formStyleArr = [];
+for(let key in formStyleObj) {
+  formStyleArr.push(key);
+}
+for(let i = 0; i < formStyleArr.length; i++) {
+  formLoginPageWrapper.children[0].style[formStyleArr[i]] = formStyleObj[formStyleArr[i]];
+}
 
-// ID input
-formLoginPageWrapper.children[0].children[0].style.marginBottom = '50px';
-formLoginPageWrapper.children[0].children[0].style.height = '52px';
-formLoginPageWrapper.children[0].children[0].style.borderRadius = '52px';
-formLoginPageWrapper.children[0].children[0].style.border = 'none';
-formLoginPageWrapper.children[0].children[0].style.paddingLeft = '25px';
-formLoginPageWrapper.children[0].children[0].style.fontSize = '20px';
+// ID input 스타일
+const inputIdStyleObj = {
+  marginBottom : '50px',
+  height : '52px',
+  borderRadius : '52px',
+  border : 'none',
+  paddingLeft : '25px',
+  fontSize : '20px',
+};
+let inputIdStyleArr = [];
+for(let key in inputIdStyleObj) {
+  inputIdStyleArr.push(key);
+}
+for(let i = 0; i < inputIdStyleArr.length; i++) {
+  formLoginPageWrapper.children[0].children[0].style[inputIdStyleArr[i]] = inputIdStyleObj[inputIdStyleArr[i]];
+}
 formLoginPageWrapper.children[0].children[0].setAttribute('placeholder', ' 아이디를 입력하세요');
 
 
-// PW input
+// PW input 스타일
 formLoginPageWrapper.children[0].children[1].style.marginBottom = '80px';
 formLoginPageWrapper.children[0].children[1].style.height = '52px';
 formLoginPageWrapper.children[0].children[1].style.borderRadius = '52px';
@@ -93,32 +141,48 @@ formLoginPageWrapper.children[0].children[1].style.paddingLeft = '25px';
 formLoginPageWrapper.children[0].children[1].style.fontSize = '20px';
 formLoginPageWrapper.children[0].children[1].setAttribute('placeholder', ' 비밀번호를 입력하세요');
 
-// 로그인&회원가입 버튼 wrap
+// 로그인&회원가입 버튼 wrap 스타일
 formLoginPageWrapper.children[0].children[2].style.display = 'flex';
 formLoginPageWrapper.children[0].children[2].style.flexDirection = 'row';
 formLoginPageWrapper.children[0].children[2].style.justifyContent = 'space-between';
 formLoginPageWrapper.children[0].children[2].style.alignItems = 'center';
 
-// 로그인 input=submit 버튼
-formLoginPageWrapper.children[0].children[2].children[0].style.border = '1px solid #999';
-formLoginPageWrapper.children[0].children[2].children[0].style.borderRadius = '15px';
-formLoginPageWrapper.children[0].children[2].children[0].style.width = '180px';
-formLoginPageWrapper.children[0].children[2].children[0].style.height = '52px';
-formLoginPageWrapper.children[0].children[2].children[0].style.cursor = 'pointer';
-formLoginPageWrapper.children[0].children[2].children[0].style.fontSize = '20px';
+// 로그인 input=submit 버튼 스타일
+const submitBtnStyleObj = {
+  border : '1px solid #999',
+  borderRadius : '15px',
+  width : '180px',
+  height : '52px',
+  cursor : 'pointer',
+  fontSize : '20px',
+};
+let submitBtnStyleArr = [];
+for(let key in submitBtnStyleObj) {
+  submitBtnStyleArr.push(key);
+}
+for(let i = 0; i < submitBtnStyleArr.length; i++) {
+  formLoginPageWrapper.children[0].children[2].children[0].style[submitBtnStyleArr[i]] = submitBtnStyleObj[submitBtnStyleArr[i]];
+}
 
-
-
-// 회원가입 a태그 버튼
-formLoginPageWrapper.children[0].children[2].children[1].style.border = '1px solid #999';
-formLoginPageWrapper.children[0].children[2].children[1].style.borderRadius = '15px';
-formLoginPageWrapper.children[0].children[2].children[1].style.backgroundColor = '#d9d9d9';
-formLoginPageWrapper.children[0].children[2].children[1].style.color = '#222';
-formLoginPageWrapper.children[0].children[2].children[1].style.textDecoration = 'none';
-formLoginPageWrapper.children[0].children[2].children[1].style.width = '180px';
-formLoginPageWrapper.children[0].children[2].children[1].style.height = '52px';
-formLoginPageWrapper.children[0].children[2].children[1].style.display = 'flex';
-formLoginPageWrapper.children[0].children[2].children[1].style.justifyContent = 'center';
-formLoginPageWrapper.children[0].children[2].children[1].style.alignItems = 'center';
-formLoginPageWrapper.children[0].children[2].children[1].style.fontSize = '20px';
-formLoginPageWrapper.children[0].children[2].children[1].style.cursor = 'pointer';
+// 회원가입 a태그 버튼 스타일
+const signUpBtnStyleObj = {
+  border : '1px solid #999',
+  borderRadius : '15px',
+  backgroundColor : '#d9d9d9',
+  color : '#222',
+  textDecoration : 'none',
+  width : '180px',
+  height : '52px',
+  display : 'flex',
+  justifyContent : 'center',
+  alignItems : 'center',
+  fontSize : '20px',
+  cursor : 'pointer',
+}
+let signUpBtnStyleArr = [];
+for(let key in signUpBtnStyleObj) {
+  signUpBtnStyleArr.push(key);
+}
+for(i = 0; i < signUpBtnStyleArr.length; i++) {
+  formLoginPageWrapper.children[0].children[2].children[1].style[signUpBtnStyleArr[i]] = signUpBtnStyleObj[signUpBtnStyleArr[i]];
+}
