5a206156f13404f209227f7920c0661c9168bfe7
kimjongyoon <kimsilveryoon96@gmail.com>
2023-04-11 15:16:18 +0900

    햄버거 메뉴 생성

--- main/mainStyle.js
+++ main/mainStyle.js
@@ -14,14 +14,14 @@ function main(){
   topMenu(rootChild);
  
  
-// ----햄버거 버튼 구역입니다.----ㅇㅇ
+// ----햄버거 버튼 구역입니다.----
 
-
+// 루트 오버플로우
 styleCreate(root, {
   overflow: "hidden",
 });
 
-
+// 햄버거 설정 이곳이 메뉴 공간
   let hamburger = tagCreate("div",{});
  rootChild[0].appendChild(hamburger);
   styleCreate(hamburger,{
@@ -32,7 +32,10 @@ styleCreate(root, {
   left: "15px",
   cursor: "pointer"
   })
- 
+  
+  hamburger.addEventListener("click", () => {
+    menuSlide.style.left = "0";
+  });
 // 햄버거 바 생성
 let bar1 = tagCreate("div", {});
 let bar2 = tagCreate("div", {});
@@ -67,7 +70,7 @@ styleCreate(bar3, {
   top: "18px",
   left: "0px",
 });
-
+// 메뉴 슬라이드 요소
 let menuSlide = tagCreate("div", {});
 root.appendChild(menuSlide);
 styleCreate(menuSlide, {
@@ -80,17 +83,18 @@ styleCreate(menuSlide, {
   transition: "left 0.6s ease",
   zIndex: 1
 });
+// 햄버거 빵, 고기, 빵 이벤트 요소
+// bar1.addEventListener("click", () => {
+//   menuSlide.style.left = "0";
+// });
+// bar2.addEventListener("click", () => {
+//   menuSlide.style.left = "0";
+// });
+// bar3.addEventListener("click", () => {
+//   menuSlide.style.left = "0";
+// });
 
-bar1.addEventListener("click", () => {
-  menuSlide.style.left = "0";
-});
-bar2.addEventListener("click", () => {
-  menuSlide.style.left = "0";
-});
-bar3.addEventListener("click", () => {
-  menuSlide.style.left = "0";
-});
-
+// X버튼 요소
 let closeButton = tagCreate("div", {});
 menuSlide.appendChild(closeButton);
 styleCreate(closeButton, {
@@ -110,15 +114,62 @@ styleCreate(closeButton, {
 
 closeButton.innerHTML = "&#x2716;"; // X 모양 아이콘
 
+
+// x모양 메뉴슬라이드 클릭시,슬라이드 없어짐
 closeButton.addEventListener("click", () => {
   menuSlide.style.left = "-600px";
 });
-
+// 다른 곳 클릭시, 슬라이드 사라짐
  rootChild[1].addEventListener("click",() =>{
   menuSlide.style.left = "-600px";
 });
+// 마이페이지 버튼을 만들어준다.
+let myPageBtn = tagCreate("button", {});
+menuSlide.appendChild(myPageBtn);
+styleCreate(myPageBtn, {
+  width: "100px",
+  height: "30px",
+  backgroundColor: "#fff",
+  borderRadius: "15px",
+  position: "absolute",
+  top: "20px",
+  left: "20px",
+  cursor: "pointer",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontWeight: "bold",
+  color: "#F7786B"
+});
+myPageBtn.innerText = "마이페이지";
 
+// 로그아웃 버튼을 만들어준다.
+let logoutBtn = tagCreate("button", {});
+menuSlide.appendChild(logoutBtn);
+styleCreate(logoutBtn, {
+  width: "100px",
+  height: "30px",
+  backgroundColor: "#fff",
+  borderRadius: "15px",
+  position: "absolute",
+  top: "20px",
+  left: "140px",
+  cursor: "pointer",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontWeight: "bold",
+  color: "#F7786B"
+});
+logoutBtn.innerText = "로그아웃";
 
+myPageBtn.addEventListener("click", () => {
+  alert("마이 페이지");
+});
+
+logoutBtn.addEventListener("click", () => {
+  alert("로그아웃 페이지");
+});
 
 
 
