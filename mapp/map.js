445a9afc00744880f0175ddb243ee1f82153e174
JseopKim <jsub5646@gmail.com>
2023-04-12 12:55:48 +0900

    fr을 ot로 변경

--- mapp/map.js
+++ mapp/map.js
@@ -29,14 +29,14 @@ map.setZoomable(false);
 
 //  이미지 링크 생성을 해서 넣으니까 되었다.
 let imageSrc = 'https://i.ibb.co/zR5p1G9/dogpaw.png';
-let frImageSrc = 'https://i.ibb.co/3FMRQCr/fr-dogpaw.png' // 마커이미지의 주소입니다    
+let otImageSrc = 'https://i.ibb.co/3FMRQCr/fr-dogpaw.png' // 마커이미지의 주소입니다    
 imageSize = new kakao.maps.Size(30, 30), // 마커이미지의 크기입니다
 // imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다.
 imageOption = {offset: new kakao.maps.Point(15, 15)}; // 마커이미지의 옵션입니다.
 
 // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
 let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-let frMarkerImage = new kakao.maps.MarkerImage(frImageSrc, imageSize, imageOption);
+let otMarkerImage = new kakao.maps.MarkerImage(otImageSrc, imageSize, imageOption);
 markerPosition = new kakao.maps.LatLng(36.35, 127.385); // 마커가 표시될 위치입니다
 
 // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
@@ -128,7 +128,7 @@ function otAddMarker(position) {
   let marker = new kakao.maps.Marker({
     map: map, // 마커를 표시할 지도
     position: position, // 마커를 표시할 위치
-    image: frMarkerImage
+    image: otMarkerImage
   });
 
   // 마커가 지도 위에 표시되도록 설정합니다
