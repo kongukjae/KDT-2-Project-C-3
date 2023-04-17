24df01dfbf3d8fdc93c40deaa5d470300921368c
sumin <ujou173@gmail.com>
2023-04-17 16:41:42 +0900

    최신 댓글 보여주는 영역 구현 완료

--- post_board/dangstargram.js
+++ post_board/dangstargram.js
@@ -245,17 +245,28 @@ function postCreate(parent, src_link, text, src_comment_link) {
   styleCreate(commentViewBtnWrap, {
     position: "absolute",
     top: "5px",
-    right: "5px",
+    right: "10px",
   })
   commentViewWrap.appendChild(commentViewBtnWrap);
 
   // 댓글 수정 버튼
   const commentViewEdit = tagCreate("button", {});
+  styleCreate(commentViewEdit, {
+    backgroundColor: "transparent",
+    border: "none",
+    marginRight: "10px",
+    cursor: "pointer",
+  })
   commentViewEdit.innerText = "수정";
   commentViewBtnWrap.appendChild(commentViewEdit);
 
   // 댓글 삭제 버튼
   const commentViewDelet = tagCreate("button", {});
+  styleCreate(commentViewDelet, {
+    backgroundColor: "transparent",
+    border: "none",
+    cursor: "pointer",
+  })
   commentViewDelet.innerText = "삭제";
   commentViewBtnWrap.appendChild(commentViewDelet);
 }
