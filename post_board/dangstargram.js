37ce9eeca704e04ed77a7aceadba8d96bca50020
sumin <ujou173@gmail.com>
2023-04-17 12:41:45 +0900

    반복문을 이용해 포스트를 찍어내는데 성공

--- post_board/dangstargram.js
+++ post_board/dangstargram.js
@@ -28,7 +28,9 @@ root.appendChild(topMenuWrap);
 // topMenu(topMenuWrap);
 
 // 게시글 영역
-postCreate(root, "../resource/MainDogImg.jpg");
+for(let i = 0; i < 3; i++) { // i < 3에서 3 부분은 나중에 무한 스크롤 방식을 이용해 적용
+  postCreate(root, "../resource/MainDogImg.jpg"); // 두번째 파라미터는 DB 혹은 ftp에서 주소를 가져와서 적용, 지금은 임시 값
+}
 
 
 // 바텀 메뉴
@@ -46,6 +48,7 @@ function postCreate(parent, src_link) {
   styleCreate(postWrap, {
     border: "1px solid black",
     width: "500px",
+    marginBottom: "20px",
   })
   parent.appendChild(postWrap);
   
@@ -96,6 +99,7 @@ function postCreate(parent, src_link) {
     postBtnWrap.appendChild(postBtn);
   }
   
+  // 텍스트 영역을 감싸는 wrap, 한번 더 wrap을 이용해 감싸는 이유는 padding을 넣기 위함. 직접적으로 padding을 넣으면 paddingLeft 값 때문에 텍스트가 영역을 삐져나감
   const textWrap = tagCreate("div", {});
   styleCreate(textWrap, {
     padding: "25px",
@@ -106,12 +110,10 @@ function postCreate(parent, src_link) {
   textBox.innerText = "DB에서 받아올 텍스트 영역 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
   styleCreate(textBox, {
     border: "1px solid green",
-    width: "450px",
-    // height: "150px",
+    width: "450px", // 500px - 부모의 좌우 패딩 값
     display: "-webkit-box",
     webkitBoxOrient: "vertical",
     webkitLineClamp: "5",
-    // padding: "10px",
     overflow: "hidden",
     textOverflow: "ellipsis",
     whiteSpace: "pre-wrap",
