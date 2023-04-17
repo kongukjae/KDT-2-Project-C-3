8ea64f3781e0eb043b719580aaa38d196995cbaf
sumin <ujou173@gmail.com>
2023-04-17 12:32:49 +0900

    댕스타그램 포스트 레이아웃 뼈대 완성

--- post_board/dangstargram.js
+++ post_board/dangstargram.js
@@ -1,4 +1,4 @@
-//  import 예정 함수들, 임시로 불러옴
+//  import 예정 함수들, 임시로 가져옴
 // --------------------------------------------------------------------------------------- //
 
 function tagCreate(tType, props) {
@@ -36,6 +36,8 @@ const btmMeunWrap = tagCreate("div", {});
 btmMeunWrap.innerText = "바텀메뉴 영역";
 root.appendChild(btmMeunWrap);
 // btmMeun(rootChild[5], menuChild);
+
+
 
 // 게시글 생성 함수
 function postCreate(parent, src_link) {
@@ -91,14 +93,28 @@ function postCreate(parent, src_link) {
       width: "60px",
       height: "60px"
     })
-    postBtnWrap.appendChild(postBtn)
+    postBtnWrap.appendChild(postBtn);
   }
   
   const textWrap = tagCreate("div", {});
   styleCreate(textWrap, {
-    border: "1px solid green",
-    width: "500px",
-    height: "150px",
-  })
+    padding: "25px",
+  });
   postWrap.appendChild(textWrap);
+
+  const textBox = tagCreate("p", {});
+  textBox.innerText = "DB에서 받아올 텍스트 영역 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  styleCreate(textBox, {
+    border: "1px solid green",
+    width: "450px",
+    // height: "150px",
+    display: "-webkit-box",
+    webkitBoxOrient: "vertical",
+    webkitLineClamp: "5",
+    // padding: "10px",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "pre-wrap",
+  })
+  textWrap.appendChild(textBox);
 }
