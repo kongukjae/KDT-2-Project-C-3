b5b9adb89798966c33a632b08a6611e9ba3a772f
sumin <ujou173@gmail.com>
2023-04-18 15:52:34 +0900

    서버 연결 테스트 1

--- httpServer/backend_bottomMenu_postBoard_get.js
+++ httpServer/backend_bottomMenu_postBoard_get.js
@@ -1,6 +1,7 @@
 import htmlBox from "../common/htmlBox.js";
 import postBoardFileRead from "./backend_postBoardFileRead.js";
 import mysql from "mysql";
+import cmServer from "./commonServer.js";
 
 
 export default function postBoard(request, response){
@@ -12,9 +13,21 @@ export default function postBoard(request, response){
     response.end(htmlBox.htmlFunc(htmlBox.postBoard));
   }
   if(request.url.startsWith('/loadpostBoard')){
-    console.log(request.url);
-
+    console.log("loadpostBoard: " + request.url);
+    let nth = request.url.split('=')[1];
+    console.log("nth: " + nth);
+    let conn = mysql.createConnection(cmServer.mysqlInfo);
+    conn.connect();
+    conn.query(
+      `select * from dangstar order by date desc limit ${nth*5},5`, function(err, data) {
+        if (err) throw err;
+        else {
+          response.writeHead(200);
+          response.write(JSON.stringify(data));
+          response.end();
+        }
+      }
+    )
+    conn.end();
   }
-  
-
 }
