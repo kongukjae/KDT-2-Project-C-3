35bd1b5ae4d443c5d78948f37f06db4c77a465ce
sumin <ujou173@gmail.com>
2023-04-19 11:47:48 +0900

    임시 커밋

--- httpServer/backend_bottomMenu_postBoard_get.js
+++ httpServer/backend_bottomMenu_postBoard_get.js
@@ -5,30 +5,31 @@ import mysql from "mysql";
 import cmServer from "./commonServer.js";
 
 
-export default function postBoard(request, response){
-  console.log("요청 들어옴 2")
-  
+export default function postBoard(request, response) {
+  console.log("요청 들어옴 2");
+
   postBoardFileRead(request, response);
 
   if (request.url === "/postBoard") {
     response.writeHead(200, { "Content-Type": "text/html" });
     response.end(htmlBox.htmlFunc(htmlBox.postBoard));
   }
-  if(request.url.startsWith('/loadPostBoard')){
+  if (request.url.startsWith("/loadPostBoard")) {
     console.log(request.url);
-    let nth = request.url.split('=')[1];
+    let nth = request.url.split("=")[1];
     let conn = mysql.createConnection(cmServer.mysqlInfo);
-        conn.connect();
-        conn.query(
-          `select * from dangstar order by date desc limit ${nth*3},3`,
-          function (err, data) {
-            if (err) throw err;
-            else {
-              response.writeHead(200);
-              response.write(JSON.stringify(data));
-              response.end();
-            }}
-        );
-        conn.end();
+    conn.connect();
+    conn.query(
+      `select * from dangstar order by date desc limit ${nth * 3},3`,
+      function (err, data) {
+        if (err) throw err;
+        else {
+          response.writeHead(200);
+          response.write(JSON.stringify(data));
+          response.end();
+        }
+      }
+    );
+    conn.end();
   }
 }
