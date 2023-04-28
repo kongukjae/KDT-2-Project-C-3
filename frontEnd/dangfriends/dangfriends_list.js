77f15d28b1e06f075239140bc8a76756c4833a23
Bigbowltakestime <Bigbowltakestime@gmail.com>
2023-04-28 15:28:47 +0900

    댕프랜드 더보기 버튼 동작 모달 창 만들기

--- frontEnd/dangfriends/dangfriends_list.js
+++ frontEnd/dangfriends/dangfriends_list.js
@@ -59,7 +59,7 @@ function createfriendsList(parent, userID, dogName, intro){
   let box = tagCreate("div", {});
   parent.appendChild(box);
   styleCreate(box, dangfriendsStyle.friendsProfile);
-
+  box.style.position = 'relative'
   let profileimg = tagCreate("div", {});
   box.appendChild(profileimg);
   styleCreate(profileimg, dangfriendsStyle.profileImage);
@@ -92,8 +92,6 @@ function createfriendsList(parent, userID, dogName, intro){
   styleCreate(friendsMore, dangfriendsStyle.profileMore);
   friendsMore.innerText = '. . .';
 
-  createModalWindow(friendsMore);
-
   const xhr = new XMLHttpRequest();
   xhr.open('POST', `http://localhost:2080/sendImage`);
   xhr.responseType = 'blob';
@@ -103,27 +101,65 @@ function createfriendsList(parent, userID, dogName, intro){
     profileimg.style.backgroundImage = `url(${imageFromServer})`;
     console.log("이미지 가져오기 완료");
   });
-}
 
-function createModalWindow(friendsMore){
   // 모달창 생성
   const modal = document.createElement("div");
   modal.classList.add("modal");
-  styleCreate(modal, dangfriendsStyle.friendsMoreModal);
-
+  styleCreate(modal, {    
+    width: stylePropertyUnion.width.width120,
+    height: stylePropertyUnion.height.height126,
+    backgroundColor: stylePropertyUnion.colorTheme.white,
+    border: `3px solid ${stylePropertyUnion.colorTheme.peach}`,
+    borderRadius: stylePropertyUnion.borderRadius.borderRadius15,
+    position: "absolute",
+    top: "-10px",
+    right: "0px",
+    display: "none",
+    zIndex: "5",
+    ...stylePropertyUnion.flexColCenter
+  });
+ 
   // 버튼 생성
   const profileBtn = document.createElement("div");
-  profileBtn.textContent = "즐겨찾기 추가/ 삭제";
+  profileBtn.textContent = "프로필 보기";
   const chatBtn = document.createElement("div");
   chatBtn.textContent = "채팅";
   const reportBtn = document.createElement("div");
-  reportBtn.textContent = "차단";
+  reportBtn.textContent = "신고";
   const exitBtn = document.createElement("div");
   exitBtn.innerHTML = "&#x2716;";
   styleCreate(exitBtn, {
     display: "flex",
     justifyContent: "end",
   });
+  const jwt = document.cookie.replace(
+    /(?:(?:^|.*;\s*)jwt\s*=\s*([^;]*).*$)|^.*$/,
+    "$1"
+  );
+
+  chatBtn.addEventListener('click',()=>{
+    
+    fetch('http://localhost:2080/createChatRoomRequest', {
+      method: 'POST',
+      body: JSON.stringify({jwt:jwt,targetId:userID})
+    }).then((result)=>{
+      console.log(result);
+      let chatBoxForm = document.createElement('form');
+      chatBoxForm.method = "POST"
+      chatBoxForm.action = "/dangTalkChatRoom";
+      let params = {jwt:jwt, targetId:userID};
+      for(let key in params){
+        let hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type","hidden");
+        hiddenField.setAttribute("name",key);
+        hiddenField.setAttribute("value",params[key]);
+        chatBoxForm.appendChild(hiddenField);
+      }
+      document.body.appendChild(chatBoxForm);
+      chatBoxForm.submit();
+    })
+  })
+
 
   // 버튼을 모달창에 추가
   const modalContent = document.createElement("div");
@@ -132,11 +168,26 @@ function createModalWindow(friendsMore){
   modalContent.appendChild(profileBtn);
   modalContent.appendChild(chatBtn);
   modalContent.appendChild(reportBtn);
-  // modalContent.appendChild(mypageForm);
 
-  styleCreate(profileBtn, dangfriendsStyle.friendsMoreModalBtn);
-  styleCreate(chatBtn, dangfriendsStyle.friendsMoreModalBtn);
-  styleCreate(reportBtn, dangfriendsStyle.friendsMoreModalBtn);
+  let mypageForm = document.createElement('form');
+    
+  mypageForm.method = "POST";
+  mypageForm.action = "/mypage";
+  let params = {jwt:jwt, targetId:userID}
+  for(let key in params){
+    let hiddenField = document.createElement("input");
+    hiddenField.setAttribute("type","hidden");
+    hiddenField.setAttribute("name",key);
+    hiddenField.setAttribute("value",params[key]);
+    mypageForm.appendChild(hiddenField);
+  }
+  friendsMore.style.cursor = 'pointer'
+
+  modalContent.appendChild(mypageForm);
+
+  styleCreate(profileBtn, dangMarketStyle.marketPostAddModalBtn);
+  styleCreate(chatBtn, dangMarketStyle.marketPostAddModalBtn);
+  styleCreate(reportBtn, dangMarketStyle.marketPostAddModalBtn);
   // 모달창에 모달컨텐츠 추가
   modal.appendChild(modalContent);
 
@@ -145,16 +196,67 @@ function createModalWindow(friendsMore){
   });
 
   friendsMore.addEventListener("click", function () {
-    friendsMore.appendChild(modal);
+    box.appendChild(modal);
   });
   exitBtn.addEventListener("click", function () {
     modal.remove();
   });
-
-  // profileBtn.addEventListener("click",()=>{
-  //   mypageForm.submit();
-  // })
-
+  
+  profileBtn.addEventListener("click",()=>{
+    mypageForm.submit();
+  })
 }
+
+// function createModalWindow(friendsMore){
+//   // 모달창 생성
+//   const modal = document.createElement("div");
+//   modal.classList.add("modal");
+//   styleCreate(modal, dangfriendsStyle.friendsMoreModal);
+
+//   // 버튼 생성
+//   const profileBtn = document.createElement("div");
+//   profileBtn.textContent = "즐겨찾기 추가/ 삭제";
+//   const chatBtn = document.createElement("div");
+//   chatBtn.textContent = "채팅";
+//   const reportBtn = document.createElement("div");
+//   reportBtn.textContent = "차단";
+//   const exitBtn = document.createElement("div");
+//   exitBtn.innerHTML = "&#x2716;";
+//   styleCreate(exitBtn, {
+//     display: "flex",
+//     justifyContent: "end",
+//   });
+
+//   // 버튼을 모달창에 추가
+//   const modalContent = document.createElement("div");
+//   modalContent.classList.add("modal-content");
+//   modalContent.appendChild(exitBtn);
+//   modalContent.appendChild(profileBtn);
+//   modalContent.appendChild(chatBtn);
+//   modalContent.appendChild(reportBtn);
+//   // modalContent.appendChild(mypageForm);
+
+//   styleCreate(profileBtn, dangfriendsStyle.friendsMoreModalBtn);
+//   styleCreate(chatBtn, dangfriendsStyle.friendsMoreModalBtn);
+//   styleCreate(reportBtn, dangfriendsStyle.friendsMoreModalBtn);
+//   // 모달창에 모달컨텐츠 추가
+//   modal.appendChild(modalContent);
+
+//   exitBtn.addEventListener("mouseover", function () {
+//     exitBtn.style.cursor = "pointer";
+//   });
+
+//   friendsMore.addEventListener("click", function () {
+//     friendsMore.appendChild(modal);
+//   });
+//   exitBtn.addEventListener("click", function () {
+//     modal.remove();
+//   });
+
+//   // profileBtn.addEventListener("click",()=>{
+//   //   mypageForm.submit();
+//   // })
+
+// }
 // 하단 메뉴바
 btmMeun(rootChild[2])
