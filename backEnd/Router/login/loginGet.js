1a6eb8b1187c3283a2ae9d5ea8616700f6618046
sumin <ujou173@gmail.com>
2023-05-08 11:59:34 +0900

    로그인 페이지 접속 시 cookie 값이 있는지 확인하여 cookie가 존재 할 경우 /main으로 연결

--- backEnd/Router/login/loginGet.js
+++ backEnd/Router/login/loginGet.js
@@ -3,13 +3,22 @@ import cmServer from "../../commonServer.js";
 
 export default function callLoginGet(request, response) {
   // 들어온 요청이 GET일 경우
-  let splitURL = request.url.split("/")[2];
   if (request.url === "/") {
-    console.log(request.url);
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "text/html");
-    response.write(htmlBox.htmlFunc(htmlBox.loginBody));
-    response.end();
+    const cookieCheck = request.headers.cookie;
+    console.log(cookieCheck);
+    console.log("쿠키 체크 중")
+    if(cookieCheck !== undefined) {
+      console.log("쿠키 undefined 진입")
+      console.log("로그인 쿠키 체크");
+      response.writeHead(302, { 'Location': '/main' });
+      response.end();
+    } else {
+      console.log(request.url);
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/html");
+      response.write(htmlBox.htmlFunc(htmlBox.loginBody));
+      response.end();
+    }
   } 
   else if(request.url === "/findUserInfo"){
     response.statusCode = 200;
