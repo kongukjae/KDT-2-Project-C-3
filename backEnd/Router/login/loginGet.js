c4322310a54690538e5217094e64f680978a6355
sumin <ujou173@gmail.com>
2023-05-08 12:04:47 +0900

    회원가입 페이지와 비밀번호 찾기 페이지도 동일하게 쿠키 값이 있을 경우 /main으로 이동시키도록 구현

--- backEnd/Router/login/loginGet.js
+++ backEnd/Router/login/loginGet.js
@@ -23,9 +23,23 @@ export default function callLoginGet(request, response) {
     }
   } 
   else if(request.url === "/findUserInfo"){
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "text/html");
-    response.write(htmlBox.htmlFunc(htmlBox.findUserInfo));
-    response.end();
+    //* 요청의 header에서 cookie값 추출
+    const cookieCheck = request.headers.cookie;
+    console.log(cookieCheck);
+    console.log("쿠키 체크 중")
+    //* 쿠키가 존재한다면 /main으로 이동시킴
+    if(cookieCheck !== undefined) {
+      console.log("쿠키 undefined 진입")
+      console.log("로그인 쿠키 체크");
+      response.writeHead(302, { 'Location': '/main' });
+      response.end();
+    } else {
+      //* 쿠키 값이 없다면 그대로 비밀번호 찾기 진행
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/html");
+      response.write(htmlBox.htmlFunc(htmlBox.findUserInfo));
+      response.end();
+    }
+    
   }
 }
--- backEnd/Router/login/signupGet.js
+++ backEnd/Router/login/signupGet.js
@@ -6,9 +6,21 @@ import dupCheck from "./signupDupcheckGet.js";
 export default function signupGet(request, response) {
 
   if (request.url === "/signUp") {
-    response.writeHead(200);
+    //* 요청의 header에서 cookie값 추출
+    const cookieCheck = request.headers.cookie;
+    console.log(cookieCheck);
+    console.log("쿠키 체크 중")
+    //* 쿠키가 존재한다면 /main으로 이동시킴
+    if(cookieCheck !== undefined) {
+      console.log("쿠키 undefined 진입")
+      console.log("로그인 쿠키 체크");
+      response.writeHead(302, { 'Location': '/main' });
+      response.end();
+    } else {
+      response.writeHead(200);
     response.write(htmlBox.htmlFunc(htmlBox.signupPage));
     response.end();
+    }
   }   
   if (request.url === "/init_user/signupstyle.js") {
     cmServer.fileDirectory(`init_user/signup.js`, response);
