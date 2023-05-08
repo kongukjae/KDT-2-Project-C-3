3d41972a934d622f2d2e2f5446cc19d0c0e1a100
sumin <ujou173@gmail.com>
2023-05-08 15:31:23 +0900

    로그인/회원가입/비밀번호 찾기 페이지 접근시 토큰의 유효성 체크하는 부분을 함수로 분리시킴

--- backEnd/Router/login/signupGet.js
+++ backEnd/Router/login/signupGet.js
@@ -1,40 +1,10 @@
-import htmlBox from "../../../common/htmlBox.js";
 import cmServer from "../../commonServer.js";
 import dupCheck from "./signupDupcheckGet.js";
-import * as jwtFunc from "../../module/jsonWebToken.js";
-
+import tokenCheck from "../login/loginTokenCheck.js";
 
 export default function signupGet(request, response) {
-
   if (request.url === "/signUp") {
-    //* 요청의 header에서 cookie값 추출
-    const cookieCheck = request.headers.cookie;
-    console.log(cookieCheck);
-    console.log("쿠키 체크 중")
-    //* 쿠키가 존재한다면 /main으로 이동시킴
-    if(cookieCheck !== undefined) {
-      console.log("쿠키 !undefined 진입");
-      const checkToken = jwtFunc.jwtCheck(cookieCheck.split('jwt=')[1]).id;
-      if(checkToken === undefined) {
-        console.log("유효하지 않은 토큰입니다")
-        //* 토큰 값이 유효하지 않다면 그대로 회원가입 진행
-        console.log(request.url);
-        response.statusCode = 200;
-        response.setHeader("Content-Type", "text/html");
-        response.write(htmlBox.htmlFunc(htmlBox.findUserInfo));
-        response.end();
-      } else {
-        //* 토큰 값이 유효하다면 /main으로 이동 
-        console.log("로그인 쿠키 체크");
-        response.writeHead(302, { 'Location': '/main' });
-        response.end();
-      }
-    } else {
-      //* 쿠키 값이 없다면 그대로 회원가입 진행
-      response.writeHead(200);
-      response.write(htmlBox.htmlFunc(htmlBox.signupPage));
-      response.end();
-    }
+    tokenCheck(request, response, 'signupPage');
   }   
   if (request.url === "/init_user/signupstyle.js") {
     cmServer.fileDirectory(`init_user/signup.js`, response);
