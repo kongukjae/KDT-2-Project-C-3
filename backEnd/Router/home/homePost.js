eb57f18648779c217568232043cffdd190f9d50d
Bigbowltakestime <Bigbowltakestime@gmail.com>
2023-05-09 12:39:02 +0900

    지도 마커 수정삭제추가 Fixes #273

--- backEnd/Router/home/homePost.js
+++ backEnd/Router/home/homePost.js
@@ -11,16 +11,11 @@ export default function homePost(request, response) {
   // }
 
   if (request.url.startsWith("/slidePlease")) {
-    console.log("really");
     let body = "";
     request.on("data", function (data) {
       body = body + data;
-      console.log("black");
     });
     request.on("end", function () {
-      console.log("cute");
-
-      // let targeNumber = [4, 5, 6, 7, 8];
       let conn = mysql.createConnection(cmServer.mysqlInfo);
       conn.connect();
       conn.query(
@@ -34,10 +29,8 @@ export default function homePost(request, response) {
         function (err, data) {
           if (err) throw err;
           else {
-            console.log(data);
             response.writeHead(200, { "content-Type": "application/json" });
             response.write(JSON.stringify(data));
-            console.log("response end");
             response.end();
           }
         }
