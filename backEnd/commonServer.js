13a5548f4ec97f15c7ebdf6701dc4363d8013a85
RyuEuni <euni.ryu97@gmail.com>
2023-05-25 00:04:05 +0900

    노트북 로컬에서 실행 테스트

--- backEnd/commonServer.js
+++ backEnd/commonServer.js
@@ -2,9 +2,9 @@ import fs from "fs";
 
 const cmServer = {
   mysqlInfo: {
-    host: "ls-c563867ad0aab9958d5e4cd2ec6442cff7d907e6.cmk3l6gajyrb.ap-northeast-2.rds.amazonaws.com",
-    user: "dbmasteruser",
-    password: "HDb$_A92aHV7PF9&P)N6Iu%t*G.4ebg7",
+    host: "localhost",
+    user: "root",
+    password: "0000",
     database: "mungta",
   },
   fileDirectory: function (request, response) {
