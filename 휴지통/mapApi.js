93ff36ecdf8cd30ade9178c9467488d92cff44ab
JseopKim <jsub5646@gmail.com>
2023-07-05 18:26:53 +0900

    배포 -앱키 변경2

--- 휴지통/mapApi.js
+++ 휴지통/mapApi.js
@@ -34,7 +34,7 @@ const server = http.createServer(function (request, response) {
       </head>
       <body>
         <div id="map" style="width:750px;height:600px;"></div>
-        <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=cc6e89c717085e748e17665afaa2c5ac"></script>
+        <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=d806a320dceda76e2b9ad5f3a21518e4"></script>
         <script>
           var mapContainer = document.getElementById('map'), // 지도를 표시할 div
               mapOption = {
